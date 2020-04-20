279c07c27ed7ef1c8998c37f5e0c677c08b6135f
George Mbugua <gmbugua2020@gmail.com>
2020-04-19 19:23:10 -0700

    Update web_scraper.py

--- scrape_test.py
+++ scrape_test.py
@@ -1,11 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 
-# Scraping Article Headlines from test_scrape.html
-with open('test_scrape.html') as html_file:
+source = requests.get('https://coreyms.com').text
+soup = BeautifulSoup(source, 'lxml')
+article = soup.find('article')
 
-    # lxml is our parser, here we have parsed the entire file
-    soup = BeautifulSoup(html_file, 'lxml')
+headline = article.h2.a.text
+print(headline)
+
+
+########### SCRAPING FROM A LOCAL FILE ############
+
+# Scraping Article Headlines from test_scrape.html
+# with open('test_scrape.html') as html_file:
+
+# lxml is our parser, here we have parsed the entire file
+# soup = BeautifulSoup(html_file, 'lxml')
 
 # This gets me the first occurence of the article class
 # article = soup.find('div', class_='article')
@@ -18,7 +28,7 @@ with open('test_scrape.html') as html_file:
 # Instead, we want all of them
 # find_all returns an iterable, so we loop over the articles found
 # then print their headlines and summaries
-for article in soup.find_all('div', class_='article'):
-    print("HEADLINE: {}".format(article.h2.a.text))
-    print("SUMMARY: {}".format(article.p.text))
-    print()
+# for article in soup.find_all('div', class_='article'):
+#     print("HEADLINE: {}".format(article.h2.a.text))
+#     print("SUMMARY: {}".format(article.p.text))
+#     print()
--- web_scraper.py
+++ web_scraper.py
@@ -4,7 +4,6 @@ import requests
 source = requests.get('https://www.nytimes.com/section/technology').text
 soup = BeautifulSoup(source, 'lxml')
 
-# Tech Articles are here
 article_match_test = soup.find('section', id="collection-technology")
 article_matches = soup.find_all('article')
 article_authors = soup.find_all('span', class_="css-9voj2j")
@@ -17,4 +16,5 @@ for article, author_list in zip(article_matches, article_authors):
     print("HEADLINE:\n {}".format(article.h2.text))
     print("SUMMARY:\n {}".format(article.p.text))
     print("AUTHOR(S):\n {}".format(author_list))
+    print("LINK:\n https://www.nytimes.com{}".format(article.h2.a['href']))
     print()
