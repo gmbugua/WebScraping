9220a80917c0b70cbf3e8a8420414d59ac1c854c
George Mbugua <gmbugua2020@gmail.com>
2020-04-19 18:55:48 -0700

    web scraper created
    scrapes new york times technology section

---
+++ web_scraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+
+source = requests.get('https://www.nytimes.com/section/technology').text
+soup = BeautifulSoup(source, 'lxml')
+
+# Tech Articles are here
+article_match_test = soup.find('section', id="collection-technology")
+article_matches = soup.find_all('article')
+article_authors = soup.find_all('span', class_="css-9voj2j")
+
+for i in range(len(article_authors)):
+    if type(article_authors[i]) != str:
+        article_authors[i] = article_authors[i].text
+
+for article, author in zip(article_matches, article_authors):
+
+    print("HEADLINE:\n {}".format(article.h2.text))
+    print("SUMMARY:\n {}".format(article.p.text))
+    print("AUTHOR(S): ", end=" ")
+    print()
