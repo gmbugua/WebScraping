314accf7405a7c33e464f19779f6addf8e757fdd
George Mbugua <gmbugua2020@gmail.com>
2020-04-20 12:42:48 -0700

    Update web_scraper.py
    Data to csv now implemented

--- web_scraper.py
+++ web_scraper.py
@@ -1,19 +1,30 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
+import csv
 
 source = requests.get('https://www.nytimes.com/section/technology').text
 soup = BeautifulSoup(source, 'lxml')
+csv_out = open('nytimes_scrape.csv', 'w')
+csv_writer = csv.writer(csv_out)
+csv_writer.writerow(['HEADLINE', 'SUMMARY', 'ARTICLE LINK'])
 
 article_matches = soup.find_all('article')
 article_authors = soup.find_all('span', class_="css-9voj2j")
 
-for i in range(len(article_authors)):
-    if type(article_authors[i]) != str:
-        article_authors[i] = article_authors[i].text
+latest_articles = soup.find('section', id="stream-panel")
+for article in latest_articles.div.ol.find_all('li', class_='css-ye6x8s'):
+    print(article.prettify())
 
-for article, author_list in zip(article_matches, article_authors):
-    print("HEADLINE:\n {}".format(article.h2.text))
-    print("SUMMARY:\n {}".format(article.p.text))
-    print("AUTHOR(S):\n {}".format(author_list))
-    print("LINK:\n https://www.nytimes.com{}".format(article.h2.a['href']))
-    print()
+# for i in range(len(article_authors)):
+#     if type(article_authors[i]) != str:
+#         article_authors[i] = article_authors[i].text
+
+# for article, author_list in zip(article_matches, article_authors):
+#     h = article.h2.text
+#     s = article.p.text
+#     l = f"https://www.nytimes.com{article.h2.a['href']}"
+#     csv_writer.writerow([h, s, l])
+
+# csv_out.close()
+# csv_in = pd.read_csv('cms_scrape.csv')
